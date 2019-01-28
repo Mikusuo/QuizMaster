bd2ab5d1cefbdb63adc52a41cfc33b57720daaf9
Mikusuo <m_suokas@outlook.com>
2019-01-28 13:29:51 +0200

    Master koodi

--- QuizMerge/Program.cs
+++ QuizMerge/Program.cs
@@ -10,6 +10,15 @@ namespace QuizMerge
     {
         static void Main(string[] args)
         {
+            //  1.Repositorion luonti githubiin ja collaborator statuksen lisäys muille ryhmäläisille. 
+            //    2.Mergetkää kaikki pelit samaan projektiin.  
+            //    3.Refaktoroikaa koodia siten, että kaikki pelit toimivat samassa projektissa.Pelissä tulee olla mahdollisuus
+            //    valita mitä tietovisa peleistä pelaatte. 
+            //    4.Voitte rakentaa lisää luokkia tai tehdä muita tarvittavia muutoksia jotta saatte kaikki pelit toimimaan.
+
+
+
+
         }
     }
 }
